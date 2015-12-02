14e85dbe2d8fa740d2684e3f7b16fe9c60779c18
Raphael Medaer <rme@escaux.com>
2015-12-02 18:05:32 +0100

    Moved Playbook object to YAMLFile Implements variables in hosts

--- example.js
+++ example.js
@@ -1,34 +1,72 @@
 var Cluster = require('./').Cluster;
 
 var host;
+var hostname = 'database.example.com';
 
 var c = new Cluster('tmp/escaux');
 
 c.initialize()
+
+// Create a new host
 .then(function() {
-    return c.createHost('database.example.com');
+    return c.createHost(hostname);
 })
 .then(function(_host) {
     host = _host;
+    return Promise.resolve();
+})
+
+// Add a host in the cluster
+.then(function() {
     return c.addHost(host);
 })
+
+// Retrieve a Host object from its name
 .then(function() {
+    return c.host(host.name());
+})
+
+// Set host variables
+.then(function() {
+    return host.setVariables({
+        test: {
+            key: 'value',
+            number: 42
+        },
+        array: [
+            'item'
+        ]
+    });
+})
+
+// Get host variables
+.then(function() {
+    return host.variables();
+})
+.then(function(variables) {
+    console.log('Variables:', variables);
+    return Promise.resolve();
+})
+
+
+.then(function() {
+    // return Promise.reject('Pause');
     return c.hosts();
 })
 .then(function(hosts) {
-    console.log(hosts);
+    console.log('Hosts:', hosts);
     return c.removeHost(host);
 })
 .then(function() {
     return c.hosts();
 })
 .then(function(hosts) {
-    console.log(hosts);
+    console.log('Hosts:', hosts);
     return c.destroyHost(host.name());
 })
 .then(function() {
     console.log('Finished');
 })
 .catch(function(err) {
-    console.log(err.stack);
+    console.log('Issue:', err.stack);
 })
